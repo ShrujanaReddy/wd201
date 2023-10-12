477bbbb19c9d9b45949f4e2af1e106313cbab813
ShrujanaReddy <130744023+ShrujanaReddy@users.noreply.github.com>
2023-10-12 20:31:41 +0530

    Update todos.js

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -3,148 +3,80 @@ var cheerio=require("cheerio")
 const db=require("../models/index")
 const app=require("../app")
 let server,agent
-
 function extractCsrfToken(res) {
   var $=cheerio.load(res.text)
   return $("[name=_csrf]").val()
 }
-describe("Todo test suite",  () => {
-  beforeAll(async () => {
-    await db.sequelize.sync({ force: true });
-    server = app.listen(5000, () => {});
-    agent = request.agent(server);
+describe("Todo test suite",  ()=> {
+     beforeAll(async ()=>{
+        await db.sequelize.sync({force:true})
+        server=app.listen(5000,()=>{ })
+        agent=request.agent(server)
+     });
+     afterAll(async ()=>{
+        await db.sequelize.close();
+        server.close()
+     })
+     test("creates new todo",async () => {
+      const res=await agent.get("/")
+      const csrfToken=extractCsrfToken(res)
+        const response=await agent.post('/todos').send({
+            'title':'buy ',
+            dueDate:new Date().toISOString(),
+            completed:false,
+            "_csrf":csrfToken
+        });
+        expect(response.statusCode).toBe(302);
+     })
+     test("Not create a todo item with an empty dueDate", async () => {
+  const res = await agent.post("/todos").send({
+    title: "Task with Empty Due Date",
+    dueDate: "",
+    completed: false,
   });
-  afterAll(async () => {
-    await db.sequelize.close();
-    server.close();
-  });
-
-  test("creates new todo", async () => {
-    const res = await agent.get("/");
-    const csrfToken = extractCsrfToken(res);
-    const response = await agent.post('/todos').send({
-      title: 'buy',
-      dueDate: new Date().toISOString(),
-      completed: false,
-      "_csrf": csrfToken,
-    });
-    expect(response.statusCode).toBe(302); // 302 Found (Redirect after successful creation)
-  });
-
-  test("Not create a todo item with an empty dueDate", async () => {
-    const res = await agent.get("/");
-    const csrfToken = extractCsrfToken(res);
-    const response = await agent.post("/todos").send({
-      title: "Task with Empty Due Date",
-      dueDate: "",
-      completed: false,
-      "_csrf": csrfToken,
-    });
-    expect(response.status).toBe(422); // 400 Bad Request
-  });
-
-  test("Create a sample task due today", async () => {
-    const res = await agent.get("/");
-    const csrfToken = extractCsrfToken(res);
-    const response = await agent.post("/todos").send({
-      title: "Complete Today's Task",
-      dueDate: new Date().toISOString().split("T")[0],
-      completed: false,
-      "_csrf": csrfToken,
-    });
-
-    expect(response.status).toBe(302); // 302 Found (Redirect after successful creation)
-  });
-
-  test("Creating a sample task due later", async () => {
-    const futureDate = new Date();
-    futureDate.setDate(futureDate.getDate() + 1);
-    const res = await agent.get("/");
-    const csrfToken = extractCsrfToken(res);
-    const response = await agent.post("/todos").send({
-      title: "Plan for Tomorrow",
-      dueDate: futureDate.toISOString().split("T")[0],
-      completed: false,
-      "_csrf": csrfToken,
-    });
-    expect(response.status).toBe(302); // 302 Found (Redirect after successful creation)
-  });
-
-  test("Create a sample overdue task", async () => {
-    const pastDate = new Date();
-    pastDate.setDate(pastDate.getDate() - 1);
-    const res = await agent.get("/");
-    const csrfToken = extractCsrfToken(res);
-    const response = await agent.post("/todos").send({
-      title: "Yesterday's Task",
-      dueDate: pastDate.toISOString().split("T")[0],
-      completed: false,
-      "_csrf": csrfToken,
-    });
-    expect(response.status).toBe(302); // 302 Found (Redirect after successful creation)
-  });
-
-  test("Delete a todo item", async () => {
-  // Create a new todo
-  const res = await agent.get("/");
-  const csrfToken = extractCsrfToken(res);
-  const createTodoResponse = await agent.post("/todos").send({
-    title: "Todo to Delete",
-    dueDate: new Date().toISOString().split("T")[0],
+  expect(res.status).toBe(500); 
+});
+  test('marks a todo as complete', async () => {
+  // First, create a to-do item
+  let res = await agent.get("/");
+  let csrfToken = extractCsrfToken(res);
+  const createResponse = await agent.post('/todos').send({
+    title: 'Buy groceries',
+    dueDate: new Date().toISOString(),
     completed: false,
     "_csrf": csrfToken,
   });
-
-  // Check if the todo was created successfully and get its ID
-  let todoId = null;
-  if (createTodoResponse.status === 500) { 
-    // Extract the ID from the response header location
-    //const locationHeader = createTodoResponse.header.location;
-    //todoId = Number(locationHeader.split("/").pop());
-  }
-
-  // If we have a valid todo ID, attempt to delete it
-  if (todoId !== null) {
-    const deleteResponse = await agent.delete(`/todos/${todoId}`).send();
-
-    // Check the status code for the delete operation
-    expect(deleteResponse.status).toBe(500); 
-  } 
+  expect(createResponse.statusCode).toBe(201); 
+  const createdTodo = createResponse.body; 
+  const markCompleteResponse = await agent.put(`/todos/${createdTodo.id}`).send({
+    completed: true,
+    "_csrf": csrfToken,
+  });
+  expect(markCompleteResponse.statusCode).toBe(200);
+  const markedTodoResponse = await agent.get(`/todos/${createdTodo.id}`);
+  expect(markedTodoResponse.statusCode).toBe(200); 
+  const markedTodo = markedTodoResponse.body; 
+  expect(markedTodo.completed).toBe(true);
 });
 test("Mark a sample overdue task as completed", async () => {
   const pastDate = new Date();
   pastDate.setDate(pastDate.getDate() - 1);
-  const res = await agent.get("/");
-  const csrfToken = extractCsrfToken(res);
-  // Create a task
   const createTaskResponse = await agent.post("/todos").send({
     title: "Yesterday's Task",
     dueDate: pastDate.toISOString().split("T")[0],
     completed: false,
-    "_csrf": csrfToken,
   });
-  expect(createTaskResponse.status).toBe(302); // 302 Found (Redirect after successful creation)
+  expect(createTaskResponse.status).toBe(200); 
+  const taskId = createTaskResponse.body.id;  
 
-  // Get the ID of the created task
-  const locationHeader = createTaskResponse.header.location;
-  const taskId = Number(locationHeader.split("/").pop());
 
-  // Mark the task as completed
   const markCompletedResponse = await agent.put(`/todos/${taskId}`).send({
     completed: true,
   });
-  expect(markCompletedResponse.status).toBe(500); // Update the status code to 200 OK (Successful update)
+  expect(markCompletedResponse.status).toBe(200); 
 
-  // Retrieve the list of all todos
-  const getTodosResponse = await agent.get("/todos");
-  expect(getTodosResponse.status).toBe(200); // 200 OK
 
-  // Find the created todo in the list by its ID
-  const createdTodo = getTodosResponse.body.find((todo) => todo.id === taskId);
-
-  // Ensure that the todo has been marked as completed
-  //expect(createdTodo).toBeDefined();
-  //expect(createdTodo.completed).toBe(true);
-});
-
+  const getTaskResponse = await agent.get(`/todos/${taskId}`);
+  expect(getTaskResponse.status).toBe(200);
+  expect(getTaskResponse.body.completed).toBe(true);
 });
